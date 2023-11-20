f358b3f4978bfa2ba7fc2ee99599ca88b6e23b38
root <root@moodle-vm.us-central1-a.c.st-moodle-project.internal>
2023-11-20 08:15:09 +0000

    adding config.php

---
+++ moodle/config.php
@@ -0,0 +1,27 @@
+<?php  // Moodle configuration file
+
+unset($CFG);
+global $CFG;
+$CFG = new stdClass();
+
+$CFG->dbtype    = 'mysqli';
+$CFG->dblibrary = 'native';
+$CFG->dbhost    = 'localhost';
+$CFG->dbname    = 'moodle';
+$CFG->dbuser    = 'rootmoodleuser';
+$CFG->dbpass    = 'Bruhpasswd4rootmoodleuser!';
+$CFG->prefix    = 'mdl_';
+$CFG->dboptions = array (
+  'dbpersist' => 0,
+  'dbport' => '',
+  'dbsocket' => '',
+  'dbcollation' => 'utf8mb4_unicode_ci',
+);
+
+$CFG->wwwroot   = 'http://35.184.177.7';
+$CFG->dataroot  = '/var/moodledata';
+$CFG->admin     = 'admin';
+
+$CFG->directorypermissions = 0777;
+
+require_once(__DIR__ . '/lib/setup.php');
