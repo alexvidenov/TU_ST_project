38621820443eaa819da9d13295dad0c962d81f0e
David georgiv <dadid1234@gmail.com>
2023-11-29 10:39:29 +0200

    fix

--- moodle/local/remote_sync/classes/observer.php
+++ moodle/local/remote_sync/classes/observer.php
@@ -2,11 +2,9 @@
 
 class local_remote_sync_observer
 {
-    public static function remote_sync(\core\event\user_loggedin $event)
+    public static function remote_sync(\core\event\user_created $event)
     {
         $event_data = $event->get_data();
-        var_dump($event_data);
         error_log("in observer");
-        die();
     }
 }
--- moodle/local/remote_sync/db/events.php
+++ moodle/local/remote_sync/db/events.php
@@ -3,7 +3,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $observers = array(
     array(
-        'eventname' => 'core\event\user_loggedin',
+        'eventname' => 'core\event\user_created',
         'callback' => 'local_remote_sync_observer::remote_sync',
     ),
     
