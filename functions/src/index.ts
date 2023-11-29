7a97d4bf86a0d07a3cc4abeec28a63914684ab37
Parakatowski <a.videnov8a@gmail.com>
2023-11-29 17:45:16 +0200

    initial user sync API (untested)

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,19 +1,7 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * import {onCall} from "firebase-functions/v2/https";
- * import {onDocumentWritten} from "firebase-functions/v2/firestore";
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+import * as functions from "firebase-functions";
 
-import {onRequest} from "firebase-functions/v2/https";
-import * as logger from "firebase-functions/logger";
+import init = require("./services");
 
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
+require("./moodle_sync/index");
 
-// export const helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+export const api = functions.https.onRequest(init.app);
---
+++ functions/src/moodle_sync/index.ts
@@ -0,0 +1,1 @@
+require("./moodle_user_sync");
