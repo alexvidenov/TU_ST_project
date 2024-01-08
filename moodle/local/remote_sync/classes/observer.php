8193e5d706e567d619ab850f36ba9636cda2c4ff
David georgiv <dadid1234@gmail.com>
2024-01-08 19:41:29 +0200

    our api url

--- moodle/local/remote_sync/classes/observer.php
+++ moodle/local/remote_sync/classes/observer.php
@@ -4,18 +4,18 @@ class local_remote_sync_observer
 {
     public static function remote_sync(\core\event\user_created $event)
     {
-        // $event_data = $event->get_data();
-        // error_log("in observer");
+        //https://us-central1-moodle-tu-sync.cloudfunctions.net/api
 
         $curl= curl_init();
-        curl_setopt($curl, CURLOPT_URL, 'https://jsonplaceholder.typicode.com/posts/1');
+
+        curl_setopt($curl, CURLOPT_URL, 'https://us-central1-moodle-tu-sync.cloudfunctions.net/api/moodle/notify');
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 
-        $phoneList = curl_exec($curl);
+        $data = curl_exec($curl);
         curl_close($curl);
     
         echo "<pre>";
-        var_dump( json_decode($phoneList));
+        var_dump(json_decode($data));
         echo "<pre>";
     }
 }
